a297530d261b21b3a077bcb9ec9ee4c67ed78b6c
Nans Pellicari <nans.pellicari@gmail.com>
2020-04-10 13:34:00 +0200

    Init plugins

---
+++ Source/NansTimelineSystem/NansTimelineSystemUE4/Private/NansTimelineSystemUE4.cpp
@@ -0,0 +1,20 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "NansTimelineSystemUE4.h"
+
+#define LOCTEXT_NAMESPACE "FNansTimelineSystemUE4Module"
+
+void FNansTimelineSystemUE4Module::StartupModule()
+{
+	// This code will execute after your module is loaded into memory; the exact timing is specified in the .uplugin file per-module
+}
+
+void FNansTimelineSystemUE4Module::ShutdownModule()
+{
+	// This function may be called during shutdown to clean up your module.  For modules that support dynamic reloading,
+	// we call this function before unloading the module.
+}
+
+#undef LOCTEXT_NAMESPACE
+	
+IMPLEMENT_MODULE(FNansTimelineSystemUE4Module, NansTimelineSystemUE4)
